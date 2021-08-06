1388d4700b461c6dca2769db88e9d6b122596239
Siddhesh Yellaram <58585975+siddhesh-yellaram@users.noreply.github.com>
2021-08-07 02:05:22 +0530

    Add files via upload

---
+++ TicTacToeUI/Presentation/LoginForm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToeUI.Presentation
+{
+    class LoginForm:Form
+    {
+        public LoginForm()
+        {
+            this.Size = new Size(400, 400);
+            this.BackColor = Color.LightBlue;
+            
+            Label p1Name = new Label();
+            Label p2Name = new Label();
+            p1Name.Text = "Player 1 Name: ";
+            p2Name.Text = "Player 2 Name: ";
+            p1Name.Location = new Point(100,100);
+            p2Name.Location = new Point(100, 150);
+            p1Name.Font = new Font("Arial", 10);
+            p2Name.Font = new Font("Arial", 10);
+
+            TextBox p1Data = new TextBox();
+            TextBox p2Data = new TextBox();
+            p1Data.Location = new Point(200,100);
+            p2Data.Location = new Point(200,150);
+            p1Data.Font = new Font("Arial", 10);
+            p2Data.Font = new Font("Arial", 10);
+
+            Button submitData = new Button();
+            submitData.Text = "Submit";
+            submitData.Location = new Point(180,200);
+            submitData.Font = new Font("Arial", 10);
+            submitData.BackColor = Color.LightGreen;
+            
+            submitData.Click += (sender, e) =>
+            {
+                string p1 = p1Data.Text;
+                string p2 = p2Data.Text;
+
+                MainForm m1 = new MainForm(p1,p2);
+                m1.Show();
+            };
+
+            this.Controls.Add(p1Name);
+            this.Controls.Add(p2Name);
+            this.Controls.Add(p1Data);
+            this.Controls.Add(p2Data);
+            this.Controls.Add(submitData);
+
+        }
+    }
+}
---
+++ TicTacToeUI/Presentation/MainForm.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TicTacToeLib;
+
+namespace TicTacToeUI.Presentation
+{
+    class MainForm : Form
+    {
+        Game game;
+        Label playerName;
+        Button[] buttons = new Button[9];
+
+        public MainForm(string p1, string p2)
+        {
+            string player1Name = p1;
+            string player2Name = p2;
+            var board = new Board(3);
+            game = new Game(board);
+            game.AddPlayers(new Player(player1Name, Mark.X));
+            game.AddPlayers(new Player(player2Name, Mark.O));
+
+            this.Size = new Size(315, 369);
+
+            playerName = new Label();
+            playerName.Location = new Point(110,0);
+            playerName.Text = game.getCurrentPlayer().GetName() + " Turn!!";
+            playerName.ForeColor = Color.Green;
+            
+            var titlePanel = new Panel();
+            titlePanel.Size = new Size(315, 30);
+            titlePanel.BackColor = Color.LightSkyBlue;
+            titlePanel.Font = new Font("Arial", 10);
+            titlePanel.Controls.Add(playerName);
+            Controls.Add(titlePanel);
+
+            var buttonPanel = new Panel();
+            buttonPanel.Location = new Point(0, 30);
+            buttonPanel.Size = new Size(300, 300);
+            Controls.Add(buttonPanel);
+
+            int x = 0;
+            int y = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                if (x == 3)
+                {
+                    x = 0;
+                    y++;
+                }
+                buttons[i] = new Button();
+                buttons[i].Text = i.ToString();
+                buttons[i].Size = new Size(100, 100);
+                buttons[i].Location = new Point(100 * x, 100 * y);
+                buttons[i].Font = new Font("Arial", 13);
+                x++;
+                buttons[i].Click += GameHandler;
+                buttonPanel.Controls.Add(buttons[i]);
+            }
+        }
+        private void GameHandler(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            button.Enabled = false;
+
+            int cellIndex = Convert.ToInt32(button.Text);
+
+            if (game.getCurrentPlayer().GetMark().Equals(Mark.X))
+            {
+                button.Text = "X";
+                button.ForeColor = Color.Green;
+            }
+            else
+            {
+                button.Text = "O";
+                button.ForeColor = Color.Red;
+            }
+
+            game.Play(cellIndex);
+
+            playerName.Text = game.getCurrentPlayer().GetName() + " Turn!!";
+
+            GameResult();
+        }
+
+        private void GameResult()
+        {
+            if (game.getGameStatus().Equals(GameState.FINISHED))
+            {
+                playerName.Text = game.getNextPlayer().GetName() + " Has Won";
+            }
+            if (game.getGameStatus().Equals(GameState.DRAW))
+            {
+                playerName.Text = "Game Has Drawn";
+            }
+            if (game.getGameStatus().Equals(GameState.FINISHED) || game.getGameStatus().Equals(GameState.DRAW))
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    buttons[i].Enabled = false;
+                }
+            }
+        }
+    }
+}
